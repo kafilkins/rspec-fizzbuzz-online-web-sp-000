98a8ed1efe98b04e85f8181d75dd029345143ccf
Keith Filkins <kafilkins@me.com>
2020-11-07 20:53:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,9 @@ def fizzbuzz(int)
   if int % 5 == 0
     return "Buzz"
   end
-  if int % "3 & 5" == 0
+elsif int % 3 == 0
+    return "FizzBuzz"
+  elsif int % 5 == 0
     return "FizzBuzz"
   end
 end
